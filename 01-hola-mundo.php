36de46df1b3b37fb805aa0664969905106464445
Agner Alexis <agner1803@hotmail.com>
2021-04-07 13:37:05 -0500

    Creacion del proyecto con los archivos necesarios

---
+++ 01-hola-mundo.php
@@ -0,0 +1,17 @@
+<?php include ''?>
+
+<?php
+    echo "Hola mundo";
+?>
+
+<br>
+
+<?php echo("Hola Mundo");
+
+print("Hola Mundo");
+
+print"Hola mundo";
+
+print_r("Hola Mundo");
+
+var_dump("Hola Mundo");
