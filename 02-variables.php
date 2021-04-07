5f70e41bb3f4d4aabbc607eff2239d49da50c98d
Agner Alexis <agner1803@hotmail.com>
2021-04-07 14:42:37 -0500

    Terminé el contenido del archivo 02-variables-php

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+//Creacion de variables
+$nombre = "Agner";
+$_nombre = "Alexis";
+$nombre_ = "Alexiss";
 
+//Imprimir el contenido de una variable
+echo $nombre;
+echo $_nombre;
+echo $nombre_;
+var_dump($nombre);
 
+//Constantes
 
-include 'includes/footer.php';
+//define(nombre_constante, contenido)
+define('cons', "Este es el valor de la constante");
+
+//Imprimir el valor de una constante
+echo cons;
+
+const const2 = "ola k ase";
+echo const2;
+
+include 'includes/footer.php'; 
+?>
