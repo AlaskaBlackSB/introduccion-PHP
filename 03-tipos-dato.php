1227332189665b8fb74ae91b1b06053c5f42736f
Agner Alexis <agner1803@hotmail.com>
2021-04-07 14:48:20 -0500

    Terminé el contenido del archivo 03-tipos-datos.php

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
+//Boolean
+$boolTrue = true;
+$boolFalse = false;
 
+var_dump($boolTrue);
+var_dump($boolFalse);
+
+//Enteros
+$num = 0;
+var_dump($num);
+
+//Float
+$numFloat = 99.99;
+var_dump($numFloat);
+
+//String
+$nombre = "Agner";
+var_dump($nombre);
+
+//Arrays
+$array = [1, 2];
+var_dump($array);
 
 
 include 'includes/footer.php';
