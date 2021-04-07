406564376ccf6a52a0d89685d44a3a93403b5006
Agner Alexis <agner1803@hotmail.com>
2021-04-07 14:56:28 -0500

    Terminé el contenido del archivo 04-operadores.php

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,32 @@
 <?php include 'includes/header.php';
 
+$precio = 15;
+$cantidad = 20;
+$resultado = 0;
 
+//Suma
+$resultado = $precio + $cantidad;;
+echo $resultado;
 
+echo "<br>"; //Salto de línea
+
+//Resta
+$resultado = $precio - $cantidad;
+echo $resultado;
+
+echo "<br>"; //Salto de línea
+
+//Multiplicación
+$resultado = $precio * $cantidad;
+echo $resultado;
+
+echo "<br>"; //Salto de línea
+
+//División
+$resultado = $precio / $cantidad;
+echo $resultado;
+
+//Potencia
+$resultado = 2**3; //2^3
 
 include 'includes/footer.php';
