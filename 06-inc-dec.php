a07d3b9272d39e70dcc4485c6b9668273db04d4b
Agner Alexis <agner1803@hotmail.com>
2021-04-07 17:24:50 -0500

    Terminé el contenido de 06-inc-dec.php

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
+$cont  = 0;
 
+//Incremento
+$cont++;
+echo $cont;
 
+echo "<br>"; //Salto de línea
+
+//Incremento personalizado
+$cont+=5;
+echo $cont;
+
+echo "<br>"; //Salto de línea
+
+//Decremento
+$cont--;
+echo $cont;
+
+echo "<br>"; //Salto de línea
+
+//Decremento personalizado
+$cont-=5;
+echo $cont;
 
 include 'includes/footer.php';
