53cf796ca339687fce43f0408b1d095790921590
Agner Alexis <agner1803@hotmail.com>
2021-04-07 14:25:20 -0500

    Finalicé el contenido del archivo 01-hola-mundo.php

--- 01-hola-mundo.php
+++ 01-hola-mundo.php
@@ -1,6 +1,6 @@
-//Distintas formas de imprimir
-
-<?php include ''?>
+<!-- Distintas formas de imprimir -->
+<!-- Con PHP puedes incluir archivos html para no repetir codigo -->
+<?php include 'includes/header.php' ?>
 
 
 <?php
@@ -8,14 +8,19 @@
     echo "Hola mundo";
 ?>
 
+<!-- Puedes abrir y cerrar codigo php en cualquier momento y asi poner codigo html -->
 <br>
 
-<?php echo("Hola Mundo");
+<?php 
+    echo("Hola Mundo");
 
-print("Hola Mundo");
+    print("Hola Mundo");
 
-print"Hola mundo";
+    print"Hola mundo";
 
-print_r("Hola Mundo");
+    print_r("Hola Mundo");
 
-var_dump("Hola Mundo");
+    var_dump("Hola Mundo");
+
+    include 'includes/footer.php' 
+?>
