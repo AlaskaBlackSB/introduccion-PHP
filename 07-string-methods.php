a831c8ee9e5de61e4bf71af9ef1c4d3020fcd89c
Agner Alexis <agner1803@hotmail.com>
2021-04-07 19:42:41 -0500

    Terminé el contenido del archivo 07-string-methods.php

--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,6 +1,51 @@
 <?php include 'includes/header.php';
 
+$nombre = "    Agner :)     ";
 
+//Obtener la longitud de una cadena
+echo strlen($nombre);
 
+echo "<br>"; //Salto de línea
+
+var_dump($nombre);
+
+echo "<br>"; //Salto de línea
+
+//Eliminar espacio al inicio y al final de una cadena
+$nombreSinEspacios = trim($nombre);
+echo strlen($nombreSinEspacios);
+
+echo "<br>"; //Salto de línea
+
+//Convertir a mayúsculas
+echo strtoupper($nombreSinEspacios);;
+
+echo "<br>"; //Salto de línea
+
+//Converir a minúsculas
+echo strtolower($nombreSinEspacios);
+
+echo "<br>"; //Salto de línea
+
+//Reemplazar una cadena
+echo str_replace(':)', 'hola', $nombreSinEspacios);
+
+echo "<br>"; //Salto de línea
+
+echo "<br>"; //Salto de línea
+
+//Revisar si un String existe o no
+echo strpos($nombreSinEspacios, 'Agner');
+
+echo "<br>"; //Salto de línea
+
+//Concatenacion de Strings
+$apellido = "Amador";
+
+echo "Hola: " . $nombre . " " . $apellido;
+
+echo "<br>"; //Salto de línea
+
+echo "Hola: {$nombre} {$apellido}";
 
 include 'includes/footer.php';
