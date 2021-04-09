90ddd506e5325f24605157dae9d9f51c8885bc71
Agner Alexis <agner1803@hotmail.com>
2021-04-08 22:34:22 -0500

    Terminé el contenido del archivo 15-funciones.php

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,27 @@
-<?php include 'includes/header.php';
+<?php 
 
+//Esto nos permite declarar el modo estricto para tipos escalares en un archivo de PHP
+declare(strict_types=1);
 
+include 'includes/header.php';
 
+//Funciones
 
+//Declaracion de funciones
+
+//Parametros con valores por default
+function sumar($a=0, $b=0){
+    echo"<br>". $a + $b;
+}
+
+//Parametros en funciones
+function restar($a, $b){
+    echo "<br>". $a - $b;
+}
+
+//Llamada a las funciones
+sumar(10, 20);
+restar(10, 20);
+sumar();        //Se aplican los valores por default
 
 include 'includes/footer.php';
